b881d66b74f9f317884a03573a63856eede7f16f
Bobby <steinbach.rj@gmail.com>
2018-02-03 10:40:00 -0500

    integrated UIKit SASS files. Integrated Webpack bundler w Babel and ESLint

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const debug = process.env.NODE_ENV !== "production";
+const webpack = require('webpack');
+const path = require('path');
+
+
+const productionPlugins = [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+];
+
+module.exports = {
+    context: __dirname,
+
+    devtool: debug ? "inline-sourcemap" : null,
+
+    entry: {
+        core: path.resolve(__dirname, "assets/js/core.js")
+    },
+
+    output: {
+        path: path.resolve(__dirname, "assets/dist"),
+        filename: "[name].min.js"
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+
+    plugins: debug ? [] : productionPlugins
+};
