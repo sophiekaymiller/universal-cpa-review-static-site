d1a995d238ceacc58fa61a0ec9346d4e38cd5c89
Bobby <steinbach.rj@gmail.com>
2018-02-15 00:43:25 -0500

    finish v1 of Contact us page design implementation

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,16 +1,34 @@
 import postToZapierWebhook from './lib/zaps';
-import { serializeFormToObject } from './lib/forms';
+import { serializeFormToObject, setupValidation } from './lib/forms';
 
 
-$('.ucr-section-hero form').on('submit', evt => {
+const $form = $('.ucr-section-hero form');
+const validation = setupValidation($form, true);
+
+// on submit of form, send the form data to zapier
+$form.on('submit', evt => {
     evt.preventDefault();
+    evt.stopPropagation();
 
-    const $form = $(evt.currentTarget);
     postToZapierWebhook(
         'https://hooks.zapier.com/hooks/catch/2938588/zmi2b1/',
         serializeFormToObject($form)
     )
     .then(() => {
-        $form.find('.alert').addClass('show');
+        $form.find('.alert').removeClass('d-none').addClass('show');
     });
+
+    return false;
+});
+
+// since the validation doesnt catch the scenario where i never keydown in an input (for inline validation), we
+// require the use of the 'ucr-nokeys' class to determine (initially) whether a field should have a valid indicator
+$form.find('.form-group.ucr-field-indicator').on('keyup', evt => {
+    $(evt.currentTarget).removeClass('ucr-nokeys');
+
+    const isValidPromise = validation.validateSection($form.get(0));
+    // isValidPromise resolves with true if the form is valid, or a list of the invalid fields if invalid
+    isValidPromise.then(isValid => {
+        isValid === true ? $form.find('.btn').removeAttr('disabled') : $form.find('.btn').attr('disabled', true);
+    })
 });
