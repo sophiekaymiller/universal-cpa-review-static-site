85a2450291c04c44d0ca87b8495154345438e937
Bobby <steinbach.rj@gmail.com>
2018-02-17 12:38:13 -0500

    added versioning to static files (via VERSION env var)

---
+++ _plugins/environment_variables.rb
@@ -0,0 +1,11 @@
+module Jekyll
+  class EnvironmentVariablesGenerator < Generator
+    def generate(site)
+      site.config['env'] = {}
+
+      ENV.each do |key, value|
+        site.config['env'][key] = value
+      end
+    end
+  end
+end
